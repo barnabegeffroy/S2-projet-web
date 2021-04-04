75170ee402e9db78bdf0f435b2f5786af8115884
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-05 00:36:57 +0200

    Try to fix errors

--- src/utils/autoloader.php
+++ src/utils/autoloader.php
@@ -3,6 +3,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include_once '../src/Model/Repository/PersonRepository.php';
+include_once '../src/Model/Repository/UserRepository.php';
 include_once '../src/Model/Factory/dbFactory.php';
-include_once '../src/Model/Entity/Person.php';
+include_once '../src/Model/Entity/User.php';
+include_once '../src/Model/Hydrator/UserHydrator.php';
