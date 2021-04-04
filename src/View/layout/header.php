659426ddc36c8ed45605b6bb18423af3a99f3859
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-05 00:49:09 +0200

    Fix q variable

--- src/View/layout/header.php
+++ src/View/layout/header.php
@@ -47,7 +47,7 @@ if(isset($_GET['q']) AND !empty($_GET['q'])){
 /*Si aucun résultat n'est trouvé, on affiche aucun résultat*/
 else{ ?>
 <div class="recherche_fail">
-    Aucun résultat pour : "<?= $q?>"
+    Aucun résultat pour : "<?= $_GET['q']?>"
 <div>
 <?php
 }
