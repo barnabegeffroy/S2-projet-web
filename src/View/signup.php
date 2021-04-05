a2f74ce20d0c0d5cc817b4f89467361dede46cfd
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-05 10:46:14 +0200

    Add user info to signup form

--- src/View/signup.php
+++ src/View/signup.php
@@ -1,19 +1,32 @@
 <form action="addUser.php" method="post">
   <div>
-    <label>Email :</label>
-    <input type="text" name="email" />
-    <?php if (isset($data['userAlreadyExist'])): ?>
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" name="prenom" required />
+  </div>
+  <div>
+    <label for="nom">Nom :</label>
+    <input type="text" id="nom" name="nom" required />
+  </div>
+  <div>
+    <label for="email">Email :</label>
+    <input type="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" required />
+    <?php if (isset($data['userAlreadyExist'])) : ?>
       <span class="error-message"><?= $data['userAlreadyExist'] ?></span>
     <?php endif; ?>
   </div>
   <div>
+    <label for="phone">Numéro de téléphone:</label>
+    <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required>
+    <small>Format: 0612345678</small>
+  </div>
+  <div>
     <label>Mot de passe :</label>
-    <input type="password" name="password" />
+    <input type="password" name="password" required />
   </div>
   <div>
     <label>Vérification du mot de passe :</label>
-    <input type="password" name="password_verify" />
-    <?php if (isset($data['passwordDoesNotMatch'])): ?>
+    <input type="password" name="password_verify" required />
+    <?php if (isset($data['passwordDoesNotMatch'])) : ?>
       <span class="error-message"><?= $data['passwordDoesNotMatch'] ?></span>
     <?php endif; ?>
   </div>
