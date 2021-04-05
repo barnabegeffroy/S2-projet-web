59f0a58ba08d1cf8893c0f00b674a1cdc35260af
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-05 18:01:51 +0200

    Add path

--- public/connexion/logout.php
+++ public/connexion/logout.php
@@ -1,7 +1,7 @@
 <?php
-include_once '../src/utils/autoloader.php';
+include_once '../../src/utils/autoloader.php';
 session_destroy();
 
-header('Location: index.php'); 
+header('Location: ../index.php'); 
 
 ?>
