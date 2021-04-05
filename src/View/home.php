a8ac4bdccd5a18cf0208e9300320b046d19d414a
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-05 17:45:05 +0200

    Imporve #5

--- src/View/home.php
+++ src/View/home.php
@@ -1,1 +1,7 @@
+<?php
+$erreur = !empty($_GET['erreur']) ? $_GET['erreur'] : null;
+if (null != $erreur): ?>
+<p class="error"><?php echo $erreur; ?></p>
+<?php
+endif;?>
 <h1>Découvrez votre signe astrologique !</h1>
