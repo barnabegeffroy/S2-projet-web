dbaabf2c4d4c0cbeac2bd628a592b037e0c6c88b
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-05 11:46:35 +0200

    add login

---
+++ public/authenticate.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../src/utils/autoloader.php';
+include_once '../src/View/template.php';
+
+$dbfactory = new \Rediite\Model\Factory\dbFactory();
+$dbAdapter = $dbfactory->createService();
+$userHydrator = new \Rediite\Model\Hydrator\UserHydrator();
+$userRepository = new \Rediite\Model\Repository\UserRepository($dbAdapter, $userHydrator);
+$userService = new \Rediite\Model\Service\UserService($userRepository);
+
+$email =  $_POST['email'];
+$password =  $_POST['password'];
+
+$viewData = [];
+$user = $userRepository->findOneByEmail($email);
+if (null !== $user && password_verify($password, $user->getPassword())) {
+    $_SESSION['user_id'] = $user->getId();
+    header('Location: index.php');
+    exit;
+}
+$viewData['failedAuthent'] = 'Authentication failed';
+loadView('login', $viewData);
--- src/View/login.php
+++ src/View/login.php
@@ -1,14 +1,15 @@
 <form action="authenticate.php" method="post">
   <div>
-    <label>Email :</label>
-    <input type="text" name="email" />
+    <label for="email">Email :</label>
+    <input type="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" required />
+
   </div>
   <div>
-    <label>password :</label>
-    <input type="password" name="password" />
+    <label>Mot de passe :</label>
+    <input type="password" name="password" required />
   </div>
-  <?php if (isset($data['failedAuthent'])): ?>
-      <span class="error-message"><?= $data['failedAuthent'] ?></span>
-    <?php endif; ?>
+  <?php if (isset($data['failedAuthent'])) : ?>
+    <span class="error-message"><?= $data['failedAuthent'] ?></span>
+  <?php endif; ?>
   <button type="submit">Valider</button>
 </form>
