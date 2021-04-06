3caf8612bf97efbc4d3cc3c78564702e100917b6
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-06 09:30:40 +0200

    Last try ?

--- src/Model/Repository/UserRepository.php
+++ src/Model/Repository/UserRepository.php
@@ -39,7 +39,6 @@ class UserRepository
     $stmt->bindValue(':telephone', $telephone, \PDO::PARAM_STR);
     $stmt->bindValue(':password', $password, \PDO::PARAM_STR);
     $stmt->execute();
-    $rawUser = $stmt->fetch();
   }
 
   function findOneByEmail($email): ?UserEntity
