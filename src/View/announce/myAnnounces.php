6b1943f6e0446ee87eb8fb497b9d189def4140ff
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-08 16:34:56 +0200

    Try #9

--- src/View/announce/myAnnounces.php
+++ src/View/announce/myAnnounces.php
@@ -27,7 +27,7 @@ $announces = $announceRepository->findAllByUserId($_SESSION['user_id']);
         <form action="deleteAnnounce.php" method="post" class="form-container">
             <label for="password"><b>Mot de passe</b></label>
             <input type="password" placeholder="entrez votre mot de passe" name="password" required>
-            <input name="idAnnounce" type="hidden" value="-1">
+            <input name="idAnnounce" type="hidden">
             <button type="submit" class="button1">Supprimer définitivement</button>
             <button type="button" class="button1 cancel" onclick="closeForm()">Annuler</button>
         </form>
