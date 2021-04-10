8a54003f7519eac56a7463fe27fe6d033cdec048
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-10 18:54:02 +0200

    try curl request

--- public/addAnnounce.php
+++ public/addAnnounce.php
@@ -23,10 +23,39 @@ if (null !== $adresse) {
   $url = "https://api-adresse.data.gouv.fr/search/?q=" . $adresse . "&" . $cp;
 } else
   $url = "https://api-adresse.data.gouv.fr/search/?q=" . $ville . "&type=street&" . $cp;
+$response = get_web_page($url);
+$resArr = array();
+$resArr = json_decode($response);
+echo "<pre>";
+print_r($resArr);
+echo "</pre>";
 
+function get_web_page($url)
+{
+  $options = array(
+    CURLOPT_RETURNTRANSFER => true,   // return web page
+    CURLOPT_HEADER         => false,  // don't return headers
+    CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+    CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+    CURLOPT_ENCODING       => "",     // handle compressed
+    CURLOPT_USERAGENT      => "test", // name of client
+    CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+    CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+    CURLOPT_TIMEOUT        => 120,    // time-out on response
+  );
+
+  $ch = curl_init($url);
+  curl_setopt_array($ch, $options);
+
+  $content  = curl_exec($ch);
+
+  curl_close($ch);
+
+  return $content;
+}
 
 if (null !== $titre &&  null !== $date) {
-  $announceRepository->insert($titre, $_SESSION['user_id'], $date, $duree, $description, $url);
+  $announceRepository->insert($titre, $_SESSION['user_id'], $date, $duree, $description, $response);
   if ($image !== null) {
     $dossier = '../src/View/images/announces/';
     $fichier = basename($image['name']);
