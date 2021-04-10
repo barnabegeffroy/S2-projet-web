199cc674ee03acb778f1d0c95b0048bbfe5ad1f7
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-10 09:28:47 +0200

    Try #4

--- public/addAnnounce.php
+++ public/addAnnounce.php
@@ -29,21 +29,24 @@ if (null !== $titre &&  null !== $date) {
     //Début des vérifications de sécurité...
     if (!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
     {
-      $viewData['errorInCreation'] += 'Vous devez uploader un fichier de type png, gif, jpg ou jpeg';
-    }
-    if ($taille > $taille_maxi) {
-      $viewData['errorInCreation'] += 'Le fichier est trop gros...';
-    }
-    if (!isset($viewData['errorInCreation'])) //S'il n'y a pas d'erreur, on upload
-    {
+      $viewData['errorInCreation'] = 'Vous devez uploader un fichier de type png, gif, jpg ou jpeg';
+    } else if ($taille > $taille_maxi) {
+      $viewData['errorInCreation'] = 'Le fichier est trop gros...';
+    } else {
       $id = $announceRepository->getLastCreated($_SESSION['user_id'])['id'];
-      $fichier = $id . '.' . $extension;
+      $fichier = $id . $extension;
       if (move_uploaded_file($image['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
       {
-        $viewData['errorInCreation'] += 'Upload effectué avec succès !';
+        if (isset($viewData['errorInCreation']))
+          $viewData['errorInCreation'] += 'Upload effectué avec succès !';
+        else
+          $viewData['errorInCreation'] = 'Upload effectué avec succès !';
       } else //Sinon (la fonction renvoie FALSE).
       {
-        $viewData['errorInCreation'] += 'Echec de l\'upload !';
+        if (isset($viewData['errorInCreation']))
+          $viewData['errorInCreation'] += 'Echec de l\'upload !';
+        else
+          $viewData['errorInCreation'] = 'Echec de l\'upload !';
       }
     }
   }
