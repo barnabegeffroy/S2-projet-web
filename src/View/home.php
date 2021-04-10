eb0ca7ab2e8a1b751c5b9ddd1f6f48b03e40e89d
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-04-10 13:11:55 +0200

    Try #2

--- src/View/home.php
+++ src/View/home.php
@@ -18,16 +18,10 @@ $announces = $announceRepository->findAll();
             if ($announce->getUserId() ==  $_SESSION['user_id']) : ?>
                 <button class="button1" onclick="location.href = 'myAnnounces.php'">Voir toutes mes annonces</button>
             <?php else :
-                if ($announceService->isFav($announce->getUserId(), $_SESSION['user_id'])) {
-                    $file = "addToFav.php";
-                    $text = "Ajouter aux";
-                } else {
-                    $file = "deleteFav.php";
-                    $text = "Supprimer des";
-                } ?>
-                <form action="<?php echo $file ?>" method="POST">
+                $bool = ($announceService->isFav($announce->getId(), $_SESSION['user_id'])) ?>
+                <form action="<?php echo $bool ? "addToFav.php" : "deleteFav.php" ?>" method="POST">
                     <input type="hidden" name="id" value="<?php echo $announce->getId() ?>">
-                    <button class="button1" type="submit"><?php echo $text ?> favoris</button>
+                    <button class="button1" type="submit"><?php echo $bool ? "Ajouter aux" : "Supprimer des" ?> favoris</button>
                 </form>
     <?php endif;
         endif;
