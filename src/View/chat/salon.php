abf74a2fd914d8fe1f5c3225aefeb5f6805fa744
Clémence Clavel <clemence.clavel@ensiie.fr>
2021-04-28 15:41:07 +0200

    style

--- src/View/chat/salon.php
+++ src/View/chat/salon.php
@@ -6,31 +6,11 @@ $messages = ""
     <head>
         
         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+
 
     </head>
     <body>
-        <style> 
-        /*Rectangle qui contient les messages */
-        .rectangle2{
-        margin-top: 10px;
-        margin-left: 50px;
-        margin-right: 50px;
-        margin-bottom: 10px;
-        padding-top : 10px;
-        padding-bottom : 10px;
-        padding-left : 10px;
-        padding-right : 10px;
-        border-top : solid;
-        border-top-color : #828282;
-        border-left : solid;
-        border-right : solid;
-        border-bottom : solid;
-        border-top-width : 3px;
-        background-color: rgb(141, 30, 104);
-        width: 300px;
-        height: 300px;
-        }
-        </style>
         <div class="rectangle2"><?php 
         if($messages!=""){
         foreach($messages as $message)
