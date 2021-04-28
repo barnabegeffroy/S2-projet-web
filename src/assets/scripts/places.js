5c01994b19d41aba0506adf3227c42fa38069062
constant-gayet <gayet.constant@gmail.com>
2021-04-28 15:16:05 +0200

    try localisation

--- src/assets/scripts/places.js
+++ src/assets/scripts/places.js
@@ -1,13 +1,13 @@
 (function() {
     var placesAutocomplete = places({
-      appId: '0F5JUQOPZH',
+      appId: '0F5JUQOPZH',  
       apiKey: 'aa22f43a3845cf5d968df909b13ffd82',
       container: document.querySelector('#address')
     });
   
     var $address = document.querySelector('#address-value')
     placesAutocomplete.on('change', function(e) {
-      $address.textContent = e.suggestion.value
+      $address.value = e.suggestion.value
     });
   
     placesAutocomplete.on('clear', function() {
