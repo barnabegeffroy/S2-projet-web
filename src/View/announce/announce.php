e8a8197320386455838e0082a43dee08fb7fdc60
constant-gayet <gayet.constant@gmail.com>
2021-04-30 14:50:51 +0200

    try

--- src/View/announce/announce.php
+++ src/View/announce/announce.php
@@ -112,7 +112,7 @@ if (empty($data)) : ?>
 
 
     <div id="map"></div>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDabmvz7QF2a2kqCvs-yZjN-Uu54Ao3zbQ&callback=initMap&libraries=&v=weekly" async></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDabmvz7QF2a2kqCvs-yZjN-Uu54Ao3zbQ&callback=initMap&libraries=&v=weekly"></script>
     <script src="../src/assets/scripts/maps.js"></script>
 
     <script src="../src/assets/scripts/calendar.js"></script>
