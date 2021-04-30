42077c8b40cd322a50acedfb90279303b7f9f3be
constant-gayet <gayet.constant@gmail.com>
2021-04-30 14:52:57 +0200

    debut try

--- src/View/template.php
+++ src/View/template.php
@@ -24,8 +24,8 @@ function loadView($view, $data)
         <script src="../src/assets/scripts/bootstrap.min.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
         <script src="../src/assets/scripts/form.js"></script>
-        <script src="../src/assets/scripts/autocompletion.js"></script>
-        <script src="../src/assets/scripts/maps.js"></script>
+        <!-- <script src="../src/assets/scripts/autocompletion.js"></script> -->
+        <!-- <script src="../src/assets/scripts/maps.js"></script> -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDabmvz7QF2a2kqCvs-yZjN-Uu54Ao3zbQ&callback=initMap&libraries=places&v=weekly"></script> -->
         <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDabmvz7QF2a2kqCvs-yZjN-Uu54Ao3zbQ&callback=initMap&libraries=&v=weekly" async></script> -->
