b80882df8e175ec19d9ad10f98798d300ef6882d
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-05-02 21:57:27 +0200

    update

--- src/assets/scripts/autocompletion.js
+++
@@ -1,8 +0,0 @@
-var searchInput = 'search_input';
-
-$(document).ready(function () {
-    var autocomplete;
-    autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
-        types: ['geocode'],
-    });
-});
