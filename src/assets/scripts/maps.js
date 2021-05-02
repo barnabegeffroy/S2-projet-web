1cdfbd15ee6ffddef95e2c027b94ec9ce118ad2b
constant-gayet <gayet.constant@gmail.com>
2021-05-02 19:22:27 +0200

    + zoom

--- src/assets/scripts/maps.js
+++ src/assets/scripts/maps.js
@@ -34,7 +34,7 @@ function initMap(myLat, myLng) {
 
   // The map, centered at the announce
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 8,
     center: ensiie
   });
 
