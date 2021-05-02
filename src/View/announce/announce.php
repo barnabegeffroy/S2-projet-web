6a9af3bac601306a0510d7ba6ea583b7fd107fed
constant-gayet <gayet.constant@gmail.com>
2021-05-02 18:27:04 +0200

    loading maps asynchronously

--- src/View/announce/announce.php
+++ src/View/announce/announce.php
@@ -135,7 +135,7 @@ if (empty($data)) : ?>
     </style>
     <script src="../src/assets/scripts/maps.js"></script>
     <div id="map"></div>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDabmvz7QF2a2kqCvs-yZjN-Uu54Ao3zbQ&callback=initMap&libraries=&v=weekly"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDabmvz7QF2a2kqCvs-yZjN-Uu54Ao3zbQ&callback=initMap&libraries=&v=weekly" async></script>
 
     <script src="../src/assets/scripts/calendar.js"></script>
     <script src="../src/assets/scripts/availability.js"></script>
