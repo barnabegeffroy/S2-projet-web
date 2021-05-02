e3486e58c6d71a7cefeedf31c02e8698d24762bd
constant-gayet <gayet.constant@gmail.com>
2021-05-02 19:53:00 +0200

    maps.js update

--- src/assets/scripts/maps.js
+++ src/assets/scripts/maps.js
@@ -1,34 +1,17 @@
 var searchInput = 'search_input';
-var autocomplete;
 
 $(document).ready(function () {
-  autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+  var autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
     types: ['geocode'],
   });
-
   document.getElementById('lat').value = autocomplete.getPlace().geometry.location.lat();
   document.getElementById('lng').value = autocomplete.getPlace().geometry.location.lng();
+
 });
 
 
-// window.addEventListener('load',function(){
-//     if(document.getElementById('map')){
-//       google.load("maps", "3",{
-//         callback:function(){
-//            new google.maps.Map(document.getElementById('map'), {
-//               center: new google.maps.LatLng(0,0),
-//               zoom: 3
-//             });
-//         }
-//       });     
-//     }
-//   },false);
-
-
-
-
 // Initialize and add the map
-function initMap(myLat, myLng) {
+function initMap() {
 
   const ensiie = { lat: 48.62682123216434, lng: 2.432315013435739 };
 
@@ -38,10 +21,10 @@ function initMap(myLat, myLng) {
     center: ensiie
   });
 
-  const myPosition = { lat: myLat , lng: myLng }
-  // The marker, positioned at myPosition
-  const marker = new google.maps.Marker({
-    position: myPosition,
-    map: map,
-  });
+  // const myPosition = { lat: myLat , lng: myLng }
+  // // The marker, positioned at myPosition
+  // const marker = new google.maps.Marker({
+  //   position: myPosition,
+  //   map: map,
+  // });
 };
